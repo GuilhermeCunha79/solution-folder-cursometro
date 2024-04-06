4f3817adb93d17f23394c0d009115fd4983dcc68
Guilherme Cunha <guilhermecunha1@outlook.pt>
2024-04-06 23:14:35 +0000

    Add InstituicaoAreaCientifica

---
+++ WebApplication1/Domain/Instituicao/InstituicaoAreaCientifica.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication1.Shared;
+
+namespace WebApplication1.Domain.Instituicao;
+
+public class InstituicaoAreaCientifica : IValueObject
+{
+
+    private string Area { get; set; }
+
+    public InstituicaoAreaCientifica()
+    {
+        
+    }
+
+    public InstituicaoAreaCientifica(string area)
+    {
+        Area = area;
+    }
+}
--- WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abdfc01e9ab6ae080c9eda9f518b4b3152595f68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e08ace9ee4198b249d07b2820f615c112f96600")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
